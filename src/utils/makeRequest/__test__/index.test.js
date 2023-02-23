f397e43a4310e3a65ecb6925d38faa00c0af068b
kartik162000 <kartikg.goel@gmail.com>
2023-02-23 15:21:34 +0530

    feat: add test cases for makeRequest

---
+++ src/utils/makeRequest/__test__/index.test.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import {
+  BACKEND_URL,
+  GET_BLOG_DATA,
+  UPDATE_BLOG_DATA,
+} from "../../../constants/apiEndPoints";
+import makeRequest from "..";
+
+jest.mock("axios");
+describe("makeRequest", () => {
+  it("should make an api call with appropriate request options and return response body when only api endpoint is specified", async () => {
+    axios.mockResolvedValue({
+      data: {
+        id: 1,
+        title: "title",
+        description: "description",
+        image: "https://i.ibb.co/V38cHQ3/young-painters.png",
+        readingTime: "2mins",
+        liked: false,
+        claps: 0,
+      },
+    });
+    expect(axios).not.toBeCalled();
+    const response = await makeRequest(GET_BLOG_DATA);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      baseURL: BACKEND_URL,
+      url: "blog-posts",
+      method: "get",
+    });
+    expect(response).toEqual({
+      id: 1,
+      title: "title",
+      description: "description",
+      image: "https://i.ibb.co/V38cHQ3/young-painters.png",
+      readingTime: "2mins",
+      liked: false,
+      claps: 0,
+    });
+  });
+  it("should make api call with appropriate request options and return response body when api endpoint and request body is specified", async () => {
+    axios.mockResolvedValue({ data: { data: { claps: 1 } } });
+    expect(axios).not.toBeCalled();
+    const response = await makeRequest(UPDATE_BLOG_DATA(1), {
+      data: { claps: 1 },
+    });
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      baseURL: BACKEND_URL,
+      url: "blog-posts/1",
+      method: "put",
+      data: { claps: 1 },
+    });
+    expect(response).toEqual({ data: { claps: 1 } });
+  });
+});
